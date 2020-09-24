f09e9e45ba4974c85b0f3d3dd92c10c1b31c5843
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2020-09-24 10:00:12 +0200

    Prototype assets

---
+++ Assets/Standard Assets/2D/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+
+    public class Bullet : MonoBehaviour
+    {
+
+        public GameObject source;
+        Rigidbody2D rb;
+        // Start is called before the first frame update
+        void Start()
+        {
+            float ForceAmount = 400f;
+            rb = GetComponent<Rigidbody2D>();
+            
+            //rb.AddForce((source.transform.position - transform.position) * ForceAmount, ForceMode2D.Force);
+            rb.AddForce((transform.right) * ForceAmount, ForceMode2D.Force);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+}
